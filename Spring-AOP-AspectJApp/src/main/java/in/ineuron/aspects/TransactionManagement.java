7c3cc6ef04129ef872a0ab4e525d7cd86416dbcd
nalabolusriram <nalabolusriram@gmail.com>
2024-06-26 10:37:03 -0500

    updated

--- Spring-AOP-AspectJApp/src/main/java/in/ineuron/aspects/TransactionManagement.java
+++ Spring-AOP-AspectJApp/src/main/java/in/ineuron/aspects/TransactionManagement.java
@@ -1,36 +1,33 @@
 package in.ineuron.aspects;
+import org.springframework.stereotype.Component;
 
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TransactionManagement {
 
 	// Syntax:: AS RT packageName.ClassName.methodName(parameter)
-	@Pointcut("execution(public void in.ineuron.dao.EmployeeDao.saveEmployee())")
+	@Pointcut("execution(public * in.ineuron.dao.*.saveEmployee())")
 	public void saveOperation() {}
 	
-	@Before("") // JoinPoint
+	//Syntax:: AS RT packageName.ClassName.methodName(parameter)
+	@Pointcut("execution(public * in.ineuron.dao.*.deleteEmployee())") 
+	public void deleteOperation() {}
+	
+	@Pointcut("saveOperation() || deleteOperation()") 
+	public void operation() {}
+	
+	@Before("operation()") // JoinPoint
 	public void beginTransaction() {
 		System.out.println("Transaction begin");
 	}
 
-	@After("") // JoinPoint
+	@After("operation()") // JoinPoint
 	public void commitTransaction() {
 		System.out.println("Transaction commited...");
 	}
-
-}
-
-/*
- * //Syntax:: AS RT packageName.ClassName.methodName(parameter)
- * 
- * @Pointcut("execution(public * in.ineuron.dao.*.deleteEmployee())") public
- * void deleteOperation() {}
- * 
- * @Pointcut("saveOperation() || deleteOperation()") public void operation() {}
- */
+}  
