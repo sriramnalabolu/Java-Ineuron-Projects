76da5ca6ffd2764a05722351ef8b87a1e6a81e9b
nalabolusriram <nalabolusriram@gmail.com>
2024-07-02 11:00:23 -0500

    updated

---
+++ SpringRest-11-ConsumerApp-usingExchangeMethod/src/main/java/in/ineuron/runner/ActorServiceConsumingRunner_GettingJsonData.java
@@ -0,0 +1,38 @@
+
+package in.ineuron.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import in.ineuron.model.Actor;
+
+@Component
+public class ActorServiceConsumingRunner_GettingJsonData implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("ActorServiceConsumingRunner_pathVariable.run()");
+
+		RestTemplate template = new RestTemplate();
+		String serviceUrl = "http://localhost:8888/SpringRest-09-ProviderApp/api/actor/find/{id}";
+
+		
+		ResponseEntity<Actor> responseEntity = template.exchange(
+														serviceUrl, 
+														HttpMethod.GET, 
+														null, 
+														Actor.class, 
+														7);
+
+		
+		System.out.println("ResponseBody              :: " + responseEntity.getBody());
+		System.out.println("ResponseStatus Code Value :: " + responseEntity.getStatusCodeValue());
+		System.out.println("ResponseStatus Code       :: " + responseEntity.getStatusCode().name());
+		System.out.println("********************************************************");
+
+	}
+}
