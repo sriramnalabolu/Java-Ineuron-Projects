74b843624b1f35bf375372f814912d881526224a
nalabolusriram <nalabolusriram@gmail.com>
2024-07-03 21:38:22 -0500

    updated

---
+++ Spring AOP/Spring-AOP-AspectJApp3/src/main/java/in/ineuron/annotation/MyTransaction.java
@@ -0,0 +1,13 @@
+package in.ineuron.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME) //It indicates where the effect of annotation should be available.
+@Target(METHOD) //It indicates where this annotation can be used in code[class/Field/Method].
+public @interface MyTransaction {
+	
+}
---
+++ SpringBoot-Runner-02/src/main/java/in/ineuron/SpringBootRunner02Application.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRunner02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRunner02Application.class, args);
+	}
+
+}
