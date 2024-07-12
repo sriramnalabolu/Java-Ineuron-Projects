b6da584034a8436c0db06d61fdf2d42eda707e3c
nalabolusriram <nalabolusriram@gmail.com>
2024-07-11 21:38:29 -0500

    updated

--- Stock-UI-APP/src/main/java/in/ineuron/service/StockService.java
+++ Stock-UI-APP/src/main/java/in/ineuron/service/StockService.java
@@ -8,7 +8,7 @@ public class StockService {
 
 	public String getTotalStockPrice(String companyName, Integer quantity) {
 
-		String REST_END_POINT = "http://localhost:1111/calc/calculate/{companyName}/{quantity}";
+		String REST_END_POINT = "http://localhost:4444/api/calc/calc/calculate/{companyName}/{quantity}";
 		String response = null;
 
 		WebClient webClient = WebClient.create();
