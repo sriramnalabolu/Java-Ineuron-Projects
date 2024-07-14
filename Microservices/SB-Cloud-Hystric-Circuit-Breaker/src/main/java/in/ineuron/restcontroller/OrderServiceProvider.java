19a802b64e29a3d033e59162977e6756211ca5d6
nalabolusriram <nalabolusriram@gmail.com>
2024-07-13 21:38:30 -0500

    updated

---
+++ Microservices/SB-Cloud-Hystric-Circuit-Breaker/src/main/java/in/ineuron/restcontroller/OrderServiceProvider.java
@@ -0,0 +1,33 @@
+package in.ineuron.restcontroller;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderServiceProvider {
+
+	@GetMapping("/show")
+	@HystrixCommand(fallbackMethod = "showFallBack")
+	public String showMsg() {
+		System.out.println("OrderServiceProvider.showMsg()");
+		int a = new Random().nextInt();
+		System.out.println(a);
+		if(a<=10) {
+			throw new RuntimeException("DUMMY");
+		}
+		
+		return "Hello  From Provider";
+	}
+	
+	public String showFallBack() {
+		System.out.println("OrderServiceProvider.showFallBack()");
+		return "From FallBack Method";
+	}
+}
+
